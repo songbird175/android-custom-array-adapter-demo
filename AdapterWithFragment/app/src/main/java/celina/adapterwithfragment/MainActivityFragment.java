fcde36fd9fbc2bec36d65a97ff994c7e8d757964
songbird175 <celina.bekins@students.olin.edu>
2016-09-30 03:37:53 -0400

    ArrayAdapter code added, problem with findViewById

--- AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivityFragment.java
+++ AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -20,5 +24,15 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    private void populateUsersList() {
+        // Construct the data source
+        ArrayList<User> arrayOfUsers = User.getUsers();
+        // Create the adapter to convert the array to views
+        CustomUsersAdapter adapter = new CustomUsersAdapter(getContext(), arrayOfUsers);
+        // Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.lvUsers);
+        listView.setAdapter(adapter);
     }
 }
