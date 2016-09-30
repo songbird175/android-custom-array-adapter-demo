737be6dfa6d0d7f8bd88ddd096448f6f206756c8
songbird175 <celina.bekins@students.olin.edu>
2016-09-30 03:47:44 -0400

    ArrayAdapter works within fragment

--- AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivityFragment.java
+++ AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivityFragment.java
@@ -17,22 +17,23 @@ import butterknife.ButterKnife;
  */
 public class MainActivityFragment extends Fragment {
 
+    @BindView(R.id.lvUsers) ListView listView;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
 
-    private void populateUsersList() {
-        // Construct the data source
         ArrayList<User> arrayOfUsers = User.getUsers();
         // Create the adapter to convert the array to views
         CustomUsersAdapter adapter = new CustomUsersAdapter(getContext(), arrayOfUsers);
         // Attach the adapter to a ListView
-        ListView listView = (ListView) findViewById(R.id.lvUsers);
         listView.setAdapter(adapter);
+
+        return view;
     }
 }
