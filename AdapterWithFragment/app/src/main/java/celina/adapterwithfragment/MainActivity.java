d7befe29b52f0fc038b303116f18dbf96c87a18e
songbird175 <celina.bekins@students.olin.edu>
2016-09-29 22:29:27 -0400

    Commented out menu to deal with later

--- AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivity.java
+++ AdapterWithFragment/app/src/main/java/celina/adapterwithfragment/MainActivity.java
@@ -20,16 +20,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
+        //This creates the Floating Action Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        }); */
+        });
+        */
     }
 
+    /*
+    //This creates the menu, with its button in the top right corner
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -51,4 +56,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
